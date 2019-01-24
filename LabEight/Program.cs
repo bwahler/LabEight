9d6112c2b458909280903838407c9806e4cc992a
bwahler <bmwahler@gmail.com>
2019-01-24 10:49:46 -0500

    Update

--- LabEight/Program.cs
+++ LabEight/Program.cs
@@ -35,35 +35,38 @@ namespace LabEight
                 {"Steve", "Perrysburg", "Pizza"},
             };
 
-            Console.WriteLine("Welcome to our C# class. Which student would you like to learn more about? (Enter a number 0-14):");
-            string input = Console.ReadLine();
-            int.TryParse(input, out int userInput);
-            while (!int.TryParse(input, out userInput) || userInput > 15)
+            bool run = true;
+            while (run == true)
             {
-                Console.WriteLine("That student does not exist. Please try again. (Enter a number 0 - 14):");
-            }
-            if (int.TryParse(input, out userInput) || userInput < 15)
-            {
-                Console.WriteLine($"Student " + userInput + " is " + studentInfo[userInput, 0]);
-            }
+                Console.WriteLine("Welcome to our C# class. Which student would you like to learn more about? (Enter a number 0-14):");
+                string input = Console.ReadLine();
+                int.TryParse(input, out int userInput);
+                while (!int.TryParse(input, out userInput) || userInput > 15)
+                {
+                    Console.WriteLine("That student does not exist. Please try again. (Enter a number 0 - 14):");
+                }
+                if (int.TryParse(input, out userInput) || userInput < 15)
+                {
+                    Console.WriteLine($"Student " + userInput + " is " + studentInfo[userInput, 0]);
+                }
 
-            Console.WriteLine("What would you like to know about " + studentInfo[userInput, 0] + "?" + "(enter hometown or favorite food):");
-            string fact = Console.ReadLine().ToLower(); ;
-            //int.TryParse(fact, out int studentInput);
-            while (fact != "hometown" || fact != "favorite food" )
-            {
-                Console.WriteLine("That is not a valid data type. Please try again:");
                 Console.WriteLine("What would you like to know about " + studentInfo[userInput, 0] + "?" + "(enter hometown or favorite food):");
-                fact = Console.ReadLine().ToLower();
-                //int.TryParse(fact, out studentInput);
-            }
-            if(fact == "hometown")
-            {
-                Console.WriteLine(studentInfo[userInput, 0] + " is from " + studentInfo[userInput, 1]);
-            }
-            else if(fact == "favorite food")
-            {
-                Console.WriteLine(studentInfo[userInput, 0] + "'s favorite food is " + studentInfo[userInput, 2]);
+                string fact = Console.ReadLine().ToLower();
+                while (fact != "hometown" && fact != "favorite food")
+                {
+                    Console.WriteLine("That is not a valid data type. Please try again:");
+                    Console.WriteLine("What would you like to know about " + studentInfo[userInput, 0] + "?" + "(enter hometown or favorite food):");
+                    fact = Console.ReadLine().ToLower();
+                }
+                if (fact == "hometown")
+                {
+                    Console.WriteLine(studentInfo[userInput, 0] + " is from " + studentInfo[userInput, 1]);
+                }
+                if (fact == "favorite food")
+                {
+                    Console.WriteLine(studentInfo[userInput, 0] + "'s favorite food is " + studentInfo[userInput, 2]);
+                }
+                run = Continue();
             }
         }
 
@@ -71,22 +74,21 @@ namespace LabEight
         {
             Console.WriteLine("Would you like to learn about another student? (y/n)");
             string userContinue = Console.ReadLine().ToLower();
-            bool run;
-
+            bool execute;
             if(userContinue == "y")
             {
-                run = true;
+                execute = true;
             }
             else if (userContinue == "n")
             {
-                run = false;
+                execute = false;
             }
             else
             {
                 Console.WriteLine("That is an invalid entry. Please try again: (y/n)");
-                run = Continue();
+                execute = Continue();
             }
-            return run;
+            return execute;
         }
     }
 }
